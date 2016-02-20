485debb592d35dde2110cf4fa108ab94729597ed
Zach Schnackel <zachschnackel@gmail.com>
2016-02-20 14:19:45 -0500

    gh repo

--- gulp-tasks/deploy.js
+++ gulp-tasks/deploy.js
@@ -1,6 +1,8 @@
 module.exports = function(gulp, $, browserSync, reload, merge, paths, files) {
   return function() {
     return gulp.src('./dist/**/*')
-    .pipe($.ghPages());
+    .pipe($.ghPages({
+      'remoteUrl': 'git@github.com:zslabs/finally-teachey.git'
+    }));
   };
 };
