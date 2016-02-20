59ef74cb8b1bd4ca0df8eb3984663c3f0a5d2f96
Zach Schnackel <zachschnackel@gmail.com>
2016-02-20 14:12:53 -0500

    init

---
+++ assets/js/init.js
@@ -0,0 +1,21 @@
+(function( $ ) {
+  "use strict";
+
+  // Load SVGs
+  boomsvgloader.load('./assets/svg/sprite.svg');
+
+  // Override Smooth Scroll behavior
+  UIkit.on('beforeready.uk.dom', function() {
+    $.extend(UIkit.components.smoothScroll.prototype.defaults, {
+      duration: 600,
+      offset: 160
+    });
+  });
+
+  var timespan = countdown(new Date(), new Date('October 16, 2016 16:00:00'));
+
+  document.querySelector('.Months').innerHTML = timespan.months;
+  document.querySelector('.Days').innerHTML = timespan.days;
+  document.querySelector('.Hours').innerHTML = timespan.hours;
+  document.querySelector('.Minutes').innerHTML = timespan.minutes;
+}(jQuery));
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Basey Static
+
+Basey is a developer-friendly static site boilerplate built with [UIkit](https://github.com/uikit/uikit) and [GulpJS](http://gulpjs.com).
+
+## Customization: Getting Started
+
+* Make sure you have the following installed:
+  * [NodeJS](http://nodejs.org)
+  * [Gulp](http://gulpjs.com)
+* In your terminal, navigate to your site's directory and run the following:
+  * `npm install`
+    * Installs build modules and assets
+  * `gulp`
+     * Builds initial project
+   * `gulp watch`
+     * Watches for project changes and starts BrowserSync instance
+
+Enjoy!
